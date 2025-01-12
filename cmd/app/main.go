444b92fd63cac75a3b5109325ecc2a273412e73d
dmitry <d.neganov@orbis3.com>
2025-01-12 15:56:08 +0300

    main package import

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"go-drafts/pkg/train"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
 
 func main() {
-	fmt.Println("text")
+	defer writer.Flush()
+
+	// Чтение количества тестов
+	var t int
+	fmt.Fscanln(reader, &t)
+
+	startTests(t)
+}
+
+func startTests(t int) {
+	for i := 0; i < t; i++ {
+		train.DoFirstTest()
+	}
 }
